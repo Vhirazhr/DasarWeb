41f91edc001522ed93b9ba515b7375469fbc66b8
Vhirazhr <fakhirazahra88@gmail.com>
2024-09-24 21:35:03 +0700

    js5/php2/sec4/no1

---
+++ jobsheet5_Php2/function.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya zahra<br/>";
+    echo "Senang berkenalan degan anda<br/>";
+}
+perkenalan();
+echo "<br>"; 
+perkenalan();
+?>
