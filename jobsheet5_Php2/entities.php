9bbbc0f4ebebb66fe27bfeae585b0a8e4fc29945
Vhirazhr <fakhirazahra88@gmail.com>
2024-09-24 23:55:34 +0700

    js5/php2/sec9

---
+++ jobsheet5_Php2/entities.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Entities HTML
+        </title>
+    </head>
+    <body>
+        <p>it&#39; Time to read a HTML book.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;Keuntungan dari menggunakan nama entities:
+            sebuah nama entitas mudah di ingat. kerugian dari menggunakan namespaceentities: 
+            Browser mungkin tidak mendukung semua nama entitas, tetapi dukungan untuknomor jauh lebih baik.</p>
+            <p>&#169; 2023 jti.com</p>
+    </body>
+</html>
