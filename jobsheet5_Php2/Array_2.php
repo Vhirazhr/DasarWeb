7fc416ea7b70e9b085464c4446bcc6441e63b20c
Vhirazhr <fakhirazahra88@gmail.com>
2024-09-24 20:00:11 +0700

    js5/php2/sec2/no1

---
+++ jobsheet5_Php2/Array_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<hmtl>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <tittle>
+
+        </tittle>
+    </head>
+    <body>
+        <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdana',
+            'Domisili' => 'malang',
+            'Jenis_Kelamin' => 'Perumpuan' ];
+        
+            echo "Nama : {$Dosen ['nama']} <br>";
+            echo "Domisili : {$Dosen ['Domisili']} <br>";
+            echo "Jenis Kelamin : {$Dosen ['Jenis_Kelamin']} <br>";
+            ?>
+    </body>
+</hmtl>
