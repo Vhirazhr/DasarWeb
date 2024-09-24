d07a72a4d1637fe0fd60be1cadbc10f3afeb61de
Vhirazhr <fakhirazahra88@gmail.com>
2024-09-24 18:41:46 +0700

    js5/php2/sec1

---
+++ jobsheet5_Php2/Array_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+
+        </title>
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+        $Listdosen=["Elok nur Hamdana", "Unggul pamenang", "Bagas Nugraha"];
+
+        echo $Listdosen[2] . "<br>";
+        echo $Listdosen[0] . "<br>";
+        echo $Listdosen[1] . "<br>";
+        ?>
+    </body>
+</html>
