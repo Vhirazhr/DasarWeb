b0ede3471192ea2918515d274b8c3da1205b54cd
Vhirazhr <fakhirazahra88@gmail.com>
2024-09-24 22:00:51 +0700

    js5/php2/sec4/no5

--- jobsheet5_Php2/function_return.php
+++ jobsheet5_Php2/function_return.php
@@ -3,6 +3,13 @@ function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam.",";
+    echo "Perkenalkan, nama saya ".$nama."<br/";
 
-echo "Umur saya adalah ". hitungUmur(2003, 2024) ."tahun";
+    echo "Saya berusia".hitungUmur(2003, 2024). "tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+perkenalan("Hira");
 ?>
