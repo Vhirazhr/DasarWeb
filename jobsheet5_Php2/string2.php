1a6ab04f604e0d8338bbc6d2eb1e11b5585d898f
Vhirazhr <fakhirazahra88@gmail.com>
2024-09-24 23:31:51 +0700

    js5/php2/sec7/no2

---
+++ jobsheet5_Php2/string2.php
@@ -0,0 +1,12 @@
+<?php
+echo "Baris\nbaru";
+echo 'Baris\nbaru';
+echo "Hallo\rdunia";
+echo 'Hallo\rndunia';
+
+echo "<pre>Hallo\tdunia!</pre>";
+echo '<pre>Hallo\tdunia!</pre>';
+
+echo "Katakanlah \"Tidak pada narkoba!\" <br>";
+echo 'Katakanlah \'Tidak pada narkoba!\' <br>';
+?>
