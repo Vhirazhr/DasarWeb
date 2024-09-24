fcd7b4074a8a82a37ab2cd950040b9a89368e31f
Vhirazhr <fakhirazahra88@gmail.com>
2024-09-24 23:46:56 +0700

    js5/php2/sec7/no3

--- jobsheet5_Php2/string3.php
+++ jobsheet5_Php2/string3.php
@@ -1,4 +1,10 @@
 <?php
 $pesan = "Saya Arek Malang";
-echo strrev($pesan)."<br>";
+$pesanPerKata = explode(" ", $pesan);
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
+
+
 ?>
