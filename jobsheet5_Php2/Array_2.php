83590d058a2675874fb21357ee8263d1b593e2a2
Vhirazhr <fakhirazahra88@gmail.com>
2024-09-24 20:08:14 +0700

    js5/php2/sec2/no2

--- jobsheet5_Php2/Array_2.php
+++ jobsheet5_Php2/Array_2.php
@@ -1,22 +1,38 @@
 <!DOCTYPE html>
-<hmtl>
+<html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <tittle>
-
-        </tittle>
+        <title>Dosen Info</title>
+        <link rel="stylesheet" href="Array_2CSS.css"> 
     </head>
     <body>
         <?php
         $Dosen = [
             'nama' => 'Elok Nur Hamdana',
-            'Domisili' => 'malang',
-            'Jenis_Kelamin' => 'Perumpuan' ];
-        
-            echo "Nama : {$Dosen ['nama']} <br>";
-            echo "Domisili : {$Dosen ['Domisili']} <br>";
-            echo "Jenis Kelamin : {$Dosen ['Jenis_Kelamin']} <br>";
-            ?>
+            'Domisili' => 'Malang',
+            'Jenis_Kelamin' => 'Perempuan'
+        ];
+        ?>
+
+        <h2>Informasi Dosen</h2>
+        <table>
+            <tr>
+                <th>Field</th>
+                <th>Value</th>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td><?php echo $Dosen['nama']; ?></td>
+            </tr>
+            <tr>
+                <td>Domisili</td>
+                <td><?php echo $Dosen['Domisili']; ?></td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td><?php echo $Dosen['Jenis_Kelamin']; ?></td>
+            </tr>
+        </table>
     </body>
-</hmtl>
+</html>
