c8757814a978d0ef5d179712f4581f0d6889c33a
Vhirazhr <fakhirazahra88@gmail.com>
2024-10-08 10:41:53 +0700

    js7/sec5/no10

--- jobsheet7_Php Form Processing/regex.php
+++ jobsheet7_Php Form Processing/regex.php
@@ -1,8 +1,6 @@
 <?php
-$pattern = '/[0-9]+/';
-$text = 'There are 123 apples';
-if (preg_match($pattern, $text, $matches)) {
-    echo "Cocokkan: " . $matches[0];
-} else {
-    echo "Tidak ada yang cocok!";
-}
+$pattern = '/apple/';
+$replacement = 'banana';
+$text = 'I Like apple pie';
+$new_text = preg_replace($pattern, $replacement, $text);
+echo $new_text;
