25cde9a7505340daea7a05f55c52eb43bf9dd503
Vhirazhr <fakhirazahra88@gmail.com>
2024-10-08 10:37:48 +0700

    js7/sec5/no2

--- jobsheet7_Php Form Processing/regex.php
+++ jobsheet7_Php Form Processing/regex.php
@@ -1,8 +1,8 @@
 <?php
-$pattern = '/[a-z]/';
-$text = 'This is a Sample Text';
-if (preg_match($pattern, $text)) {
-    echo "Huruf kecil ditemukan!";
+$pattern = '/[0-9]+/';
+$text = 'There are 123 apples';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
 } else {
-    echo "Tidak ada Huruf kecil!";
+    echo "Tidak ada yang cocok!";
 }
