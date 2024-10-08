98799e74f97d78f6e4fb53a60d6f1dbddd7f22b9
Vhirazhr <fakhirazahra88@gmail.com>
2024-10-08 08:20:12 +0700

    js7/sec1/no6

--- jobsheet7_Php Form Processing/isset.php
+++ jobsheet7_Php Form Processing/isset.php
@@ -1,8 +1,15 @@
 <?php
+$data = array("nama" => "jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "nama: " . $data["nama"] . "<br>"; 
+} else {
+    echo "Variable 'nama' Tidak ditemukan dalam array";
+}
+
 $umur;
 if (isset($umur) && $umur >= 18) {
     echo "Anda sudah dewasa";
 } else {
     echo "Anda belum Dewasa atau variable 'Umur' Tidak diTemukan";
 }
-?> 
+?>
