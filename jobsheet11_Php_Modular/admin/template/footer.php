c5c9015adf71e193c2abe2f00e34a961a45928be
Vhirazhr <fakhirazahra88@gmail.com>
2024-11-20 10:42:57 +0700

    push

---
+++ jobsheet11_Php_Modular/admin/template/footer.php
@@ -0,0 +1,7 @@
+<script src="assets/dists/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.3/dist/chart.umd.js" integrity="sha384-eITPSr3LJXLISH3dJDI15YNjQ5sxrbKrCnJ7z2Xt+ENPSMOYBxl0B+l6SEzIo2L" crossorigin="anonymous"></script>
+<script src="assets/custom/dashboard.js"></script>
+
+</body>
+</html>
